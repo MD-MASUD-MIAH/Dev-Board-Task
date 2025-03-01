3dafcbb82e7bb441fa07d689183b6ff0c4742825
176409 <148116817+176409@users.noreply.github.com>
2025-03-01 22:58:44 +0600

    commt

--- js/devborad.js
+++ js/devborad.js
@@ -1,14 +1,13 @@
 const completedBtns = document.querySelectorAll(".ButtonClass");
 
-
-// compeleted buttton  
+// compeleted buttton
 
 for (let i = 0; i < completedBtns.length; i++) {
   const completedButtons = completedBtns[i];
-  
+
   completedButtons.addEventListener("click", function (event) {
     const count = document.getElementById("countText");
-    
+
     let unseenTotalText = parseInt(count.innerText);
 
     unseenTotalText = unseenTotalText - 1;
@@ -23,8 +22,9 @@ for (let i = 0; i < completedBtns.length; i++) {
 
     seen.innerText = seenTotalText;
 
+    const div = completedButtons.closest(".card");
 
-    
+    const h1 = div.querySelectorAll(".title");
     const time = new Date();
 
     const times = time.toLocaleTimeString();
@@ -35,7 +35,7 @@ for (let i = 0; i < completedBtns.length; i++) {
 
     const createElement = document.createElement("p");
     createElement.classList.add("story");
-    createElement.innerText = `You have completed task ${titles}     at ${times} `;
+    createElement.innerText = `You have completed task ${h1}     at ${times} `;
 
     historyContainer.appendChild(createElement);
 
@@ -44,52 +44,38 @@ for (let i = 0; i < completedBtns.length; i++) {
 
     alert("Board updated successfully");
 
-    if( unseenTotalText === 0){
-
-        alert('congrates!!! You have compeleted all the current task')
-        
+    if (unseenTotalText === 0) {
+      alert("congrates!!! You have compeleted all the current task");
     }
   });
 }
 
+const page = document.getElementById("nextPage");
 
+page.addEventListener("click", function (event) {
+  event.preventDefault();
 
-const page = document.getElementById('nextPage')
+  window.location.href = "./second.html";
+});
 
-page.addEventListener('click',function(event){
+const htButton = document.getElementById("HistoryButton");
 
-    event.preventDefault()
-    
-    
+htButton.addEventListener("click", function () {
+  const historyContainer = document.getElementById("historyBox");
 
-window.location.href= '../second.html'
+  historyContainer.innerText = " ";
+});
 
+const colorButton = document
+  .getElementById("colorbtn")
+  .addEventListener("click", function (event) {
+    const colorCode = "abcdef0123456789";
 
-})
+    let colorhes = "#";
 
-const htButton = document.getElementById('HistoryButton')
+    for (let i = 0; i < 6; i++) {
+      colorhes = colorhes + colorCode[Math.floor(Math.random() * 16)];
+    }
 
-htButton.addEventListener('click',function(){
-
-const historyContainer = document.getElementById('historyBox')
-
-  historyContainer.innerText= ' '
-
-
-})
-
-
-const colorButton = document.getElementById('colorbtn').addEventListener('click',function(event){
-
-const colorCode= 'abcdef0123456789'
-
-let colorhes ='#'
-
-for(let i = 0; i<6 ; i++){
-  
-  colorhes= colorhes+colorCode[Math.floor(Math.random()*16)]}
-
-
-document.getElementById('bdy').style.backgroundColor =` ${colorhes}`
-
-})
+    document.getElementById("bdy").style.backgroundColor = ` ${colorhes}`;
+  });
